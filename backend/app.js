e80cece54331804aac81bc893ac5e03128b90409
FrancisPj <francis.jub@free.fr>
2023-01-05 16:25:59 +0100

    install mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Import des routes
+const userModel = require('./models/user');
+const sauceModel = require('./models/sauce');
 
 // Configuration de la base de données mongoDB avec des variables d'environnement
-mongoose.connect('mongodb://localhost/',
+mongoose.connect('mongodb+srv://francis:xW9W8jGy9vK2Bx0d@atlascluster.ldde9ep.mongodb.net/?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
---
+++ backend/models/sauce.js
@@ -0,0 +1,32 @@
+//import de mongoose pour le schéma
+const mongoose = require('mongoose');
+//schema de données qui contient les champs souhaités pour chaque sauce
+const sauceSchema = mongoose.Schema({
+    // l'identifiant MongoDB unique de l'utilisateur qui a créé la sauce
+    userId: {type: String, required: true},
+    //nom de la sauce
+    name: { type: String, required: true},
+    // fabricant de la sauce
+    manufacturer: { type: String, required: true},
+    description: { type: String, required: true},
+    // le principal ingrédient épicé de la sauce
+    mainPepper: { type: String, required: true},
+    imageUrl: { type: String, required: true},
+    //heat: nombre entre 1 et 10 décrivant la sauce
+    heat: { type: Number, required: true},
+    likes: { type: Number, default: 0},
+    dislikes: { type: Number, default: 0},
+    //tableau des identifiants des utilisateurs qui ont aimé (= liked) la sauce <userId>
+    usersLiked: { type: [String]},
+    // tableau des identifiants des utilisateurs qui n'ont pas aimé (= disliked) la sauce <userId>
+    usersDisliked: { type: [String]}
+
+
+});
+
+//nous exportons ce schéma en tant que modèle Mongoose, le rendant par là même disponible pour notre application Express.
+module.exports = mongoose.model('Sauce', sauceSchema);
+
+
+//    La méthode "Schema" de Mongoose vous permet de créer un schéma de données pour votre base de données MongoDB.
+//     La méthode "model" transforme ce modèle en un modèle utilisable.
